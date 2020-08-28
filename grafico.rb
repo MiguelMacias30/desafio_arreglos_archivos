782dd4d8c5bd45faba24e7a7ca2c5830ee70d5a7
Miguel Macias <miguelfernandomacias@gmail.com>
2020-08-27 20:18:51 -0400

    grafica

---
+++ grafico.rb
@@ -0,0 +1,13 @@
+array = [5, 3, 2, 5, 10]
+
+array.each do |x|
+  print "|" + "**" * x
+  print "\n"
+end
+
+print ">" + "--" * array.max
+print "\n"
+
+for i in (1..array.max)
+  print "#{i} "
+end
